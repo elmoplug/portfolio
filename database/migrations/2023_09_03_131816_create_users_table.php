34880de841cab1bde223cab4b2e724ce3a316d72
elmoplug <justaway3474@gmail.com>
2023-09-04 05:40:32 +0900

    マイグレーションファイルの作成及び、テーブルの生成

---
+++ database/migrations/2023_09_03_131816_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->string('email',50);
+            $table->string('password',50);
+            $table->string('introduction',200)->nullable();
+            $table->string('image_url',200)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
--- database/migrations/2023_09_03_200802_create_reply_likes_table.php
+++ database/migrations/2023_09_03_200802_create_reply_likes_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('reply_likes', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('reply_id')->constrained('replies');
+            //$table->timestamps();
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('reply_likes');
     }
 };
