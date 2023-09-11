25644ed6e059ad8e1f077b0e2d1d50035206993c
elmoplug <justaway3474@gmail.com>
2023-09-11 23:24:09 +0900

    PostコントローラークラスとPostモデルクラスの作成、Postコントローラーへのルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/posts', [PostController::class, 'index']);
+
 require __DIR__.'/auth.php';
